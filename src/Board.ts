7fc3e150e88d69e211a01f48cac98c4917fa1da0
Evgenii Altman <altmanea@gmail.com>
2023-11-08 09:07:05 +0600

    first commit

---
+++ src/Board.ts
@@ -0,0 +1,51 @@
+import { Sym } from "./Sym"
+
+export type BoardParam = {
+    row: number,
+    col: number
+  }
+
+// Абстрактный класс доски
+export abstract class Board<T extends GameType>{
+    cells: Sym<T>[]
+
+    static row: number
+    static col: number
+
+    constructor(
+        str: Sym<T>[],
+        row?: number,
+        col?: number,
+    ) {
+        // TODO
+        // При наличии row или col 
+        //  инициализировать соответсвующие поля
+        // Инициализировать массив cells
+        this.cells = []
+    }
+
+    abstract clone(): Board<T>     
+
+    isFill(): boolean {
+        // TODO
+        // Возвращет true если на доске нет пустых клеток
+        // Реомендуется реализация без циклов,
+        //  с использованием функций массивов
+        return true
+    }
+
+    move(index: number, sym: Sym<T>): boolean {
+        // TODO
+        // Если ячейка this.cell[index] занята - возвращает false
+        // Записывает в ячейку cell и возвращает true
+        return true
+    }
+
+    status(): string { 
+        // TODO
+        // Если доска заполнена возвращает "Игра закончена"
+        //   если игра не закончена, строку "Идет игра".
+        return "Идет игра"
+    }
+
+}
